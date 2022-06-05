7bbb6d179733e985b86588d1fbe6aaf0ecfb8eb7
strongkid21 <1751862489@qq.com>
2022-06-05 22:42:43 +0800

    哨兵1.0

---
+++ build3/CMakeFiles/run.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/main4.cpp" "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/build3/CMakeFiles/run.dir/main4.cpp.o"
+  "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/src/ArmorDetector.cpp" "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/build3/CMakeFiles/run.dir/src/ArmorDetector.cpp.o"
+  "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/src/CRC_Check.cpp" "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/build3/CMakeFiles/run.dir/src/CRC_Check.cpp.o"
+  "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/src/KAL.cpp" "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/build3/CMakeFiles/run.dir/src/KAL.cpp.o"
+  "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/src/Thread.cpp" "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/build3/CMakeFiles/run.dir/src/Thread.cpp.o"
+  "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/src/camera.cpp" "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/build3/CMakeFiles/run.dir/src/camera.cpp.o"
+  "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/src/serialport.cpp" "/home/dji/Desktop/a_miaosuan_v6.3.1_new_watcher/build3/CMakeFiles/run.dir/src/serialport.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "/usr/include/eigen3"
+  "../include"
+  "../include/camera"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/run.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/main4.cpp" "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/cmake-build-debug/CMakeFiles/run.dir/main4.cpp.o"
+  "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/src/ArmorDetector.cpp" "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/cmake-build-debug/CMakeFiles/run.dir/src/ArmorDetector.cpp.o"
+  "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/src/CRC_Check.cpp" "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/cmake-build-debug/CMakeFiles/run.dir/src/CRC_Check.cpp.o"
+  "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/src/KAL.cpp" "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/cmake-build-debug/CMakeFiles/run.dir/src/KAL.cpp.o"
+  "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/src/Thread.cpp" "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/cmake-build-debug/CMakeFiles/run.dir/src/Thread.cpp.o"
+  "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/src/camera.cpp" "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/cmake-build-debug/CMakeFiles/run.dir/src/camera.cpp.o"
+  "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/src/serialport.cpp" "/home/liyankuan/CLionProjects/a_miaosuan_v6.3_new/cmake-build-debug/CMakeFiles/run.dir/src/serialport.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/eigen3"
+  "../include"
+  "../include/camera"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
