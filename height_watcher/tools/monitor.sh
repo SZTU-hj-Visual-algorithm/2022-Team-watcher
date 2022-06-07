df2872f3fd6aca85c5ed59450426d690f25eccb1
strongkid21 <1751862489@qq.com>
2022-06-07 11:57:23 +0800

    上头加上了自启动

--- height_watcher/tools/monitor.sh
+++ height_watcher/tools/monitor.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-/home/dji/Desktop/a_miaosuan_v6.3.1_new/build3/run
-export LD_LIBRARY_PATH=/home/dji/Desktop/a_miaosuan_v6.3.1_new/build3/run
+/home/nvidia/Desktop/a_miaosuan_v6.3.1_height_watcher/build/run
+export LD_LIBRARY_PATH=/home/nvidia/Desktop/a_miaosuan_v6.3.1_height_watcher/build/run
 while true; do
         server=ps aux | grep CenterServer_d | grep -v grep
         if [ ! "$server" ]; then
-            /home/dji/Desktop/a_miaosuan_v6.3.1_new/build3/run 
+            /home/nvidia/Desktop/a_miaosuan_v6.3.1_height_watche/build/run 
 	    echo "restart"
-            sleep 1
+            sleep 10
         fi
-        sleep 1
+        sleep 5
 done
